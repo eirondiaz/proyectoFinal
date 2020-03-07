e022b4416e3f69238526707609880fa90d67a0dd
Eiron <eirondiaz@gmail.com>
2020-03-07 11:00:43 -0400

    creacion del proyecto

---
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.proyectofinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.Fragments.BuscarFragment;
+import com.example.Fragments.HomeFragment;
+import com.example.Fragments.PerfilFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navbar);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_buscar:
+                            selectedFragment = new BuscarFragment();
+                            break;
+                        case R.id.nav_perfil:
+                            selectedFragment = new PerfilFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
---
+++ app/src/main/java/com/example/proyectofinal/PreLogin.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_login);
+    }
+}
