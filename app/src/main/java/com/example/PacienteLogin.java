a3292f12acb00dd9328a6e475821267e45c7e566
Eiron <eirondiaz@gmail.com>
2020-03-07 11:12:21 -0400

    cree los activities PacienteLogin y MedicoLogin

---
+++ app/src/main/java/com/example/PacienteLogin.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectofinal.R;
+
+public class PacienteLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paciente_login);
+    }
+}
--- app/src/main/java/com/example/proyectofinal/PreLogin.java
+++ app/src/main/java/com/example/proyectofinal/PreLogin.java
@@ -2,13 +2,40 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class PreLogin extends AppCompatActivity {
+import com.example.MedicoLogin;
+import com.example.PacienteLogin;
+
+public class PreLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout lyPaciente, lyMedico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
+
+        lyPaciente = findViewById(R.id.lyPaciente);
+        lyPaciente.setOnClickListener(this);
+
+        lyMedico = findViewById(R.id.lyMedico);
+        lyMedico.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.lyPaciente:
+                startActivity(new Intent(PreLogin.this, PacienteLogin.class));
+                break;
+            case R.id.lyMedico:
+                startActivity(new Intent(PreLogin.this, MedicoLogin.class));
+                break;
+        }
     }
 }
