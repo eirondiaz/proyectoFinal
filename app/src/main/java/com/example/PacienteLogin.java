cea8561ff72cae3b6ebfce0711a2a64669ed0bcf
Eiron <eirondiaz@gmail.com>
2020-03-07 13:05:41 -0400

    diseño en el LogIn

--- app/src/main/java/com/example/PacienteLogin.java
+++ app/src/main/java/com/example/PacienteLogin.java
@@ -2,15 +2,34 @@ package com.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.proyectofinal.PreLogin;
 import com.example.proyectofinal.R;
 
-public class PacienteLogin extends AppCompatActivity {
+public class PacienteLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paciente_login);
+
+        imgBack = findViewById(R.id.imgBack);
+        imgBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.imgBack:
+                startActivity(new Intent(PacienteLogin.this, PreLogin.class));
+                break;
+        }
     }
 }
