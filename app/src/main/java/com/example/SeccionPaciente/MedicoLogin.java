7cb068519e2a9f1064fa4d2eed1bd12d7171a1c9
oscarmora26 <oscarrander26>
2020-03-17 01:09:44 -0400

    LoginMedico

---
+++ app/src/main/java/com/example/SeccionPaciente/MedicoLogin.java
@@ -0,0 +1,16 @@
+package com.example.SeccionPaciente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectofinal.R;
+
+public class MedicoLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medico_login);
+    }
+}
