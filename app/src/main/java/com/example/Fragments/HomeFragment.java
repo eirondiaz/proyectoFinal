f2c7a14abc8687276038dc66378f2bb68d08faaf
oscarmora26 <oscarrander26>
2020-03-17 00:51:18 -0400

    creacion activity MedicoLogin

--- app/src/main/java/com/example/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/Fragments/HomeFragment.java
@@ -1,22 +1,35 @@
 package com.example.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.proyectofinal.R;
 
 public class HomeFragment extends Fragment {
 
+    private CardView cardViewTexto;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cardViewTexto = view.findViewById(R.id.cardViewTexto);
+        cardViewTexto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hola mundo", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return view;
     }
