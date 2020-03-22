f9f4de2ff0cc4ba57c617158a8c11859ca29129f
gabrielDox <sr.gabrielgil@gmail.com>
2020-03-22 10:47:34 -0400

    Creacion del usuario y paciente

---
+++ app/src/main/java/com/example/Usuarios/Paciente.java
@@ -0,0 +1,16 @@
+package com.example.Usuarios;
+
+import org.json.JSONObject;
+
+public class Paciente extends  Usuario{
+
+     public JSONObject BuscarMedico(){
+
+          return new JSONObject();
+     }
+
+     public void SolicitarCita(){
+
+     }
+
+}
